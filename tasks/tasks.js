bd40809f32ccfe41668fc133e7b766774861a70e
pedromn35 <a26899@sa-miranda.net>
2023-03-08 19:10:29 +0000

    the tasks

---
+++ tasks/tasks.js
@@ -0,0 +1,41 @@
+  /* 
+  
+  Fazer uma introducao aos sistemas distribuidos
+  Carateristicas (nao repudio
+  Falhas ( Falar tipos de falhas 
+ Sistemas de informacao
+  Problema a resolver da organizacao
+  Contexto de uma organizaçao
+ Arquitetura
+  Falar de alguns padroes (cliente-servidor, coisas assim...
+  
+  Distributed Systems
+   ? Caracteristics PEDRO
+   ? Types of faults DONG
+   ? Problems associated with it DONG
+   ? Global watch  MARTINS
+   ? Relate with the project
+  Organization
+   OpenUp to take advantage:
+    Initiate Project
+    // Define Vision
+        // Identify StakeHolders
+        ? Problem to be solved MARTINS
+        // Define features
+        ! System Boundaries 
+        ! Define Constraints
+    Identify and Refine Requirements
+     ? Find and Outline Requirements DONG
+          ! Requirements 
+          ! Detail use case Models 
+   Architecture
+    ? Why do we think in architecture PEDRO
+        ? Problems to be solved with it MARTINS
+    ! Architecture Requirements 
+    ! Global vision of architecture 
+    ? Layer Architecture of the project PEDRO
+
+    */
+      
+    
+        
